4eb7e9f8bed5141be75c9e19834d483e9150a11c
Asiful Haque Khan <ahkasif93@gmail.com>
2023-12-08 22:20:08 +0600

    CSS and JS added

---
+++ script.js
@@ -0,0 +1,16 @@
+// Define HTML elements
+const board = document.getElementById('game-board');
+
+//Define game variables
+let snake = [{x: 10 , y: 10}];
+
+//Draw game map, snake food
+function draw() {
+    board.innerHTML = '';
+    drawSnake():
+}
+
+//Draw Snake 
+function drawSnake (){
+
+}
